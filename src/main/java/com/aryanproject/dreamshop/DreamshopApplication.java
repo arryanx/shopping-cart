012d469c03b2219c2bf3cc100c0fa67468d3e5ac
Aryan <aryan@ARYANs-MacBook-Air-3.local>
2024-12-01 15:04:51 +0530

    init project and creating entities

---
+++ src/main/java/com/aryanproject/dreamshop/DreamshopApplication.java
@@ -0,0 +1,13 @@
+package com.aryanproject.dreamshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamshopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aryanproject/dreamshop/model/Category.java
@@ -0,0 +1,23 @@
+package com.aryanproject.dreamshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
