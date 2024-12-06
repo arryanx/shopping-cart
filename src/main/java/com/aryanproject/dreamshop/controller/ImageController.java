acea39fdbdd09aad10d669bc9a516d0e57cbf8f4
Aryan <aryan@ARYANs-MacBook-Air-3.local>
2024-12-07 02:29:27 +0530

    image controller implementation

---
+++ src/main/java/com/aryanproject/dreamshop/controller/ImageController.java
@@ -0,0 +1,76 @@
+package com.aryanproject.dreamshop.controller;
+
+import com.aryanproject.dreamshop.dto.ImageDto;
+import com.aryanproject.dreamshop.exceptions.ResourceNotFoundException;
+import com.aryanproject.dreamshop.model.Image;
+import com.aryanproject.dreamshop.response.ApiResponse;
+import com.aryanproject.dreamshop.service.image.ImageServiceInterface;
+import jakarta.validation.constraints.Null;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequestMapping("${api.prefix}/images")
+@RequiredArgsConstructor
+public class ImageController {
+    private final ImageServiceInterface imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile>files, @RequestParam Long productId){
+        try {
+            List<ImageDto>imageDtos= imageService.saveImages(files,productId);
+            return ResponseEntity.ok(new ApiResponse("Upload Success",imageDtos));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed",e.getMessage()));
+        }
+    }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image= imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getProductImage().getBytes(1, (int) image.getProductImage().length()));
+        return  ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +image.getFileName() + "\"")
+                .body(resource);
+    }
+
+    @PutMapping("/image/{imageId}/update")
+    public ResponseEntity<ApiResponse> updateImage(@RequestParam MultipartFile file, @PathVariable Long imageId){
+        try {
+            Image image= imageService.getImageById(imageId);
+            if(image != null){
+                imageService.updateImage(file,imageId);
+                return ResponseEntity.ok(new ApiResponse("Update success!", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Update failed",INTERNAL_SERVER_ERROR));
+    }
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId){
+        try{
+            Image image= imageService.getImageById(imageId);
+            if(image!=null){
+                imageService.deleteById(imageId);
+                return ResponseEntity.ok(new ApiResponse("Delete success",null));
+            }
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Delete failed",INTERNAL_SERVER_ERROR));
+    }
+}
---
+++ src/main/java/com/aryanproject/dreamshop/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.aryanproject.dreamshop.controller;
+
+public class ProductController {
+}
